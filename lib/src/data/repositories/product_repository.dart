2dcd783404f1071f2b8fda6a646949855b433457
Marcelo Figueira <marcelofigueira87@gmail.com>
2023-06-26 17:23:03 -0300

    repository update

--- lib/src/data/repositories/product_repository.dart
+++ lib/src/data/repositories/product_repository.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:api_consumer/src/data/http/exceptions.dart';
+import 'package:api_consumer/src/data/http/http_client.dart';
 import 'package:api_consumer/src/data/models/product_model.dart';
 
 abstract class IProductRepository {
@@ -5,8 +9,33 @@ abstract class IProductRepository {
 }
 
 class ProductRepository implements IProductRepository {
+  final IHttpClient client;
+
+  ProductRepository({
+    required this.client,
+  });
+
   @override
-  Future<List<ProductModel>> getProduct() {
-    throw UnimplementedError();
+  Future<List<ProductModel>> getProduct() async {
+    final response = await client.get(
+      url: 'https://dummyjson.com/products',
+    );
+
+    if (response.statusCode == 200) {
+      final List<ProductModel> products = [];
+
+      final body = jsonDecode(response.body);
+
+      body['products'].map((item) {
+        final ProductModel product = ProductModel.fromMap(item);
+        products.add(product);
+      }).toList();
+
+      return products;
+    } else if (response.statusCode == 404) {
+      throw NotFoundException('a url não foi encontrada');
+    } else {
+      throw Exception('não foi possivel carregar os produtos');
+    }
   }
 }
