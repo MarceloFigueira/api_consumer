31bbc616dd51d0bec72460bb82d1b2a6ffc690b4
Marcelo Figueira <marcelofigueira87@gmail.com>
2023-06-26 16:06:06 -0300

    flutter default app initial commit

---
+++ lib/src/services/api_app.dart
@@ -0,0 +1,19 @@
+import 'package:api_consumer/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class ApiApp extends StatelessWidget {
+  const ApiApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
