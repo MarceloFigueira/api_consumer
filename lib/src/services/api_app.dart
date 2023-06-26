a0ba87ae9372b573b4203f9674cc1fd83718bdda
Marcelo Figueira <marcelofigueira87@gmail.com>
2023-06-26 16:14:24 -0300

    remove debug Banner

--- lib/src/services/api_app.dart
+++ lib/src/services/api_app.dart
@@ -4,16 +4,16 @@ import 'package:flutter/material.dart';
 class ApiApp extends StatelessWidget {
   const ApiApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
